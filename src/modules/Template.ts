4953419b1bad554b9f1f223c8f3ca5ca04f60750
Roller Davic <fundcoincapital@gmail.com>
2022-03-13 22:40:07 +0700

    Update Template.ts

--- src/modules/Template.ts
+++ src/modules/Template.ts
@@ -16,55 +16,49 @@ const getItem = async(id:number=0) =>{
     return true;
 }
 
-const listItems = async(limit:number=8,page:number=1, search:any="") =>{
-	var sql = "";
-    try {
-    const conn = await connect();
-    if(search == ""){
-        sql = `SELECT * FROM ${dbName} ORDER BY sort_db ASC LIMIT ${limit}`;
-    }else{
-        sql = `SELECT * FROM ${dbName} WHERE ${searchField} LIKE '%"+search+"%'  ORDER BY sort_db ASC LIMIT ${limit}`;
-    }
-    const [rows, fields] = await conn.query(sql)  as any;
-    
-    return rows;
+const listItems = async() =>{
+	try {
+        const conn = await connect();
+        const [rows, fields] = await conn.query("SELECT *,DATE_FORMAT(start_time, '%d-%m-%Y %H:%i') as start_time, DATE_FORMAT(end_time, '%d-%m-%Y %H:%i') as end_time FROM template_design  ORDER BY sort_db ASC LIMIT 100")  as any;
+        return rows;
     }
     catch (e) {
         return [];
     }
+    return [];
 }
 
-const createItem = async(obj = "") =>{
-	try {
+const createItem = async(obj = {name : "", contents : "", customs_css : "", customs_javascript : "", display : "", status : "", start_time : "", end_time : "", sort_db : ""}) =>{
+    try {
         const conn = await connect();
-        var sql = `INSERT INTO ${dbName} SET ${obj}`;
-        await conn.query(sql);
+        await conn.query('INSERT INTO template_design SET name="'+obj.name+'", `contents`=\''+obj.contents+'\', `customs_css`=\''+obj.customs_css+'\', `customs_javascript`=\''+obj.customs_javascript+'\', display="'+obj.display+'", status="'+obj.status+'", start_time="'+obj.start_time+'", end_time="'+obj.end_time+'", sort_db="'+obj.sort_db+'"');
         return true;
     }
     catch (e) {
+        console.log(e);
         return false;
     }
     return true;
 }
 
-const updateItem = async(id:number=0, obj="") =>{
-	try {
+const updateItem = async(id:number=0,obj = { name : "", contents : "", customs_css : "", customs_javascript : "", display : "", status : "", start_time : "", end_time : "", sort_db : ""}) =>{
+    try {
         const conn = await connect();
-        var sql = `UPDATE ${dbName} SET ${obj} WHERE ${prikeyField}=${id}`;
-        await conn.query(sql);
+        
+        await conn.query('UPDATE template_design SET name="'+obj.name+'", `contents`=\''+obj.contents+'\', `customs_css`=\''+obj.customs_css+'\', `customs_javascript`=\''+obj.customs_javascript+'\', display="'+obj.display+'", status="'+obj.status+'", start_time="'+obj.start_time+'", end_time="'+obj.end_time+'", sort_db="'+obj.sort_db+'" WHERE id = "'+id+'"');
         return true;
     }
     catch (e) {
+        console.log(e);
         return false;
     }
     return true;
 }
 
-const deleteItem = async(id:number=0) =>{
-	try {
+const deleteItem = async function(id:number = 0) {
+    try {
     const conn = await connect();
-    var sql = `DELETE FROM ${dbName} WHERE ${prikeyField}=${id}`;
-    await conn.query(sql);
+    await conn.query('DELETE FROM template_design WHERE id="'+id+'"');
     return true;
     }
     catch (e) {
